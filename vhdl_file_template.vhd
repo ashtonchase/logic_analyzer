182ca4f2aa34e3f9a3f002fa382d2ace34a1e99f
ashtonchase <acj0003@gmail.com>
2016-02-22 20:03:17 -0600

    added template VHDL file. added capture_ctrl_e entity file

---
+++ vhdl_file_template.vhd
@@ -0,0 +1,32 @@
+-------------------------------------------------------------------------------
+-- Title      : Human Readable Name for the module in this file. 
+-- Project    : fpga_logic_analyzer
+-------------------------------------------------------------------------------
+-- File       : vhdl_file_template.vhd
+-- Created    : 2016-02-22
+-- Last update: 2016-02-22
+-- Standard   : VHDL'08
+-------------------------------------------------------------------------------
+-- Description: This is where you will describe this file
+-------------------------------------------------------------------------------
+-- Copyright (c) 2016 Ashton Johnson, Paul Henny, Ian Swepston, David Hurt
+-------------------------------------------------------------------------------
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+-- GNU General Public License for more details.
+
+-- You should have received a copy of the GNU General Public License along
+-- with this program; if not, write to the Free Software Foundation, Inc.,
+-- 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+-------------------------------------------------------------------------------
+-- Revisions  :
+-- Date        Version      Author      Description
+-- 2016-02-22      1.0      (your name)	    Created
+-------------------------------------------------------------------------------
+
