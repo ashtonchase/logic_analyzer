82bb98239b22aa27473f878623c96b2b9d17799d
dch0006 <thedavidhurt@gmail.com>
2016-04-04 19:50:50 -0500

    Added state machine skeleton
    Message processor almost complete.

--- src/msg_processor.vhd
+++ src/msg_processor.vhd
@@ -4,7 +4,7 @@
 -------------------------------------------------------------------------------
 -- File       : msg_processor.vhd
 -- Created    : 2016-03-17
--- Last update: 2016-03-31
+-- Last update: 2016-04-04
 -- Standard   : VHDL'08
 -------------------------------------------------------------------------------
 -- Description: The message processor waits for the UART module to provide 
@@ -31,8 +31,9 @@
 -------------------------------------------------------------------------------
 -- Revisions  :
 -- Date        Version      Author      Description
--- 2016-03-17      0.0      David	      Created
--- 2016-03-31      0.1      David	      Entity done
+-- 2016-03-17      0.0      David	    Created
+-- 2016-03-31      0.1      David	    Entity done
+-- 2016-04-04	   0.2      David       State machine in progress
 -------------------------------------------------------------------------------
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
@@ -44,15 +45,15 @@ entity msg_processer is
 		rst : in std_logic;  -- Synchronous reset		
 		
 		-- UART Interface
-		data_in	 : in std_logic_vector(7 downto 0);  -- Byte of command/data from UART
-		data_new : in std_logic;  -- Strobe to indicate new byte
+		byte_in	 : in std_logic_vector(7 downto 0);  -- Byte of command/data from UART
+		byte_new : in std_logic;  -- Strobe to indicate new byte
 
 		-- Sample Rate Control Interface
 		sample_f : out std_logic_vector(31 downto 0);  -- Sampling frequency to Sample Rate Control
 		
 		-- Capture Control Interface
 		reset     : out std_logic;  -- Reset capture control
-		arm       : out std_logic;  -- Arm capture control
+		armed     : out std_logic;  -- Arm capture control
 		read_cnt  : out std_logic(15 downto 0);  -- Number of samples (divided by 4) to send to memory
 		delay_cnt : out std_logic(15 downto 0);  -- Number of samples (divided by 4) to capture after trigger
 		trig_msk  : out std_logic(31 downto 0);  -- Define which trigger values must match
@@ -63,19 +64,80 @@ entity msg_processer is
 end entity msg_processor;
 
 architecture behave of msg_processor is
-	
+	signal cmd_in  : STD_LOGIC_VECTOR(7 downto 0) := (others => '0');
+	signal data_in : STD_LOGIC_VECTOR(31 downto 0) := (others => '0');
 
+	type state_t is (INIT, READ_CMD, DO_CMD, BYTE1, BYTE2, BYTE3, BYTE4);
+	signal state : state_t;
 
-
-
-
-
-
-
-
-
-
-
+begin
+	process(clk)
+	begin
+		if rising_edge(clk) then
+			if rst = '1' then
+				state <= INIT;
+				sample_f <= 0;
+				reset <= '0';
+				armed <= '0';
+				read_cnt <= x"0000";
+				delay_cnt <= x"0000";
+				trig_msk <= x"00000000";
+				trig_vals <= x"00000000";
+			else
+				case state is
+					when INIT =>
+						if byte_new = '1' then
+							cmd_in <= byte_in;
+							state <= READ_CMD;
+						end if;
+					when READ_CMD =>
+						if to_integer(unsigned(cmd_in)) <= x"13" then
+							state <= DO_CMD;  -- Short command
+						else
+							state <= WORD1;   -- Long command
+						end if;
+					when BYTE1 =>
+						if byte_new = '1' then
+							data_in(7 downto 0) <= byte_in;
+							state <= BYTE2;
+						end if;
+					when BYTE2 =>
+						if byte_new = '1' then
+							data_in(15 downto 8) <= byte_in;
+							state <= BYTE3;
+						end if;
+					when BYTE3 =>
+						if byte_new = '1' then
+							data_in(23 downto 16) <= byte_in;
+							state <= BYTE4;
+						end if;
+					when BYTE4 =>
+						if byte_new = '1' then
+							data_in(31 downto 24) <= byte_in;
+							state <= DO_CMD;
+						end if;
+					when DO_CMD =>
+						case cmd_in is
+							when x"00" =>		-- Reset
+								reset <= '1';
+							when x"01" =>		-- Run
+								armed <= '1';
+							when x"02" =>		-- ID (unimplemented)
+							when x"11" =>		-- XON (unimplemented)
+							when x"13" =>   -- XOFF (unimplemented)
+							when x"C0" | x"C4" | x"C8" | x"CC" =>		-- Set Trigger Mask
+							when x"C1" | x"C5" | x"C9" | x"CD" =>		-- Set Trigger Values
+							when x"C2" | x"C6" | x"CA" | x"CE" =>		-- Set Trigger Configuration (unimplemented)
+							when x"80" =>		-- Set Divider
+							when x"81" =>		-- Set Read & Delay Count
+							when x"82" =>		-- Set Flags
+						end case;
+						state <= INIT;
+				end case;
+			end if;
+		end if;
+	end process;
+end architecture;
 
 
 
